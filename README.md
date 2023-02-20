1f77dd193afe6879a5cd54865468abd688f4f226
mattobcodes <muchoperro77@gmail.com>
2023-02-20 23:38:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Monday Meeting Randomizer
+
+A randomizer for **finding a moderator for the monday meeting** (or digital meetings in general)
+
+## Description
+
+When we have our regular online meeting it's always the question **who moderates the meeting**. 
+Since you can not flip a coin for a group of people and we have anyways a lot to decide,
+i made this **Monday Meeting Randomizer**!
+
+It has preconfigured list of names, but you can also **add new names** on the spot.
+Before you start the randomization, you can also **uncheck the boxes** left of the names,
+if people are absent.
+
+Then start the randomization and the fun begins!
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ script.js
@@ -0,0 +1,139 @@
+const mondayList = document.getElementById('monday-list');
+const newPeople = document.getElementById('new-people');
+const submitBtn = document.getElementById('submit-btn');
+const startBtn = document.getElementById('start-btn');
+const listContainer = document.getElementById('list-container');
+const randomContainer = document.getElementById('random-container');
+const randomNames = document.getElementById('random-names');
+const emojiField = document.getElementById('emoji-area');
+const moderator = document.getElementById('moderator');
+
+// init names
+const mondayPeople = [
+    'Flavius',
+    'Bertha',
+    'Lea',
+    'Desmond',
+    'Nikita',
+    'Ulf',
+    'Miguel'
+]
+
+// store listitems
+const listItems = [];
+
+// store the actual names for randomization
+const listForRandomization = [];
+
+//init count variable
+let count = 0;
+
+// create list out of existing names and append to monday-list
+function createList() {
+
+    [...mondayPeople]
+        .forEach((person) => {
+            const listItem = document.createElement('li');
+
+            // create element
+            listItem.innerHTML = `<input type="checkbox" checked="checked" class="list-item" id="${count}">
+            <span class="name" id="${count}">${person}</span>`;
+
+            // append element
+            mondayList.appendChild(listItem);
+            listItems.push(listItem);
+            count++;
+        })
+}
+
+// add new name to monday list
+function addPeople() {
+    if (newPeople.value !== '') {
+
+        // create element
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<input type="checkbox" checked="checked" class="list-item" id="${count}">
+        <span class="name" id="${count}">${newPeople.value}</span>`;
+
+        //append element
+        mondayList.appendChild(listItem);
+        listItems.push(listItem);
+
+        // clear input and increment counter
+        newPeople.value = '';
+        count++;
+    }
+    else {
+        console.log('Please add a name');
+    }
+}
+
+// check list for radio buttons before starting the randomizer
+function checkList() {
+
+    // collect all names in the list
+    const allItems = document.querySelectorAll(".list-item");
+    const allNames = document.querySelectorAll(".name");
+
+    // loop through all names and check if radio button is checked and add to new list
+    for (let i = 0; i < allItems.length; i++) {
+        if (allItems[i].checked) {
+            listForRandomization.push(allNames[i].innerHTML);
+        }
+    }
+}
+
+//start randomizer
+function startRandomizer() {
+    checkList();
+    listContainer.style.display = 'none';
+    randomContainer.style.display = 'flex';
+    randomNames.style.display = 'flex';
+    randomContainer.classList.add("random-container-open");
+    randomLoop();
+}
+
+// start list
+createList();
+
+// eventlisteners for buttons
+submitBtn.onclick = addPeople;
+startBtn.onclick = startRandomizer;
+
+/* randomizer */
+
+// random function
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// emoji array
+var emojis = [
+    '😄', '😃', '😀', '😊', '☺', '😉', '😍', '😘', '😚', '😗', '😙', '😜', '😝', '😛', '😳', '😁', '😔', '😌', '😒', '😞', '😣', '😢', '😂', '😭', '😪', '😥', '😰', '😅', '😓', '😩', '😫', '😨', '😱', '😠', '😡', '😤', '😖', '😆', '😋', '😷', '😎', '😴', '😵', '😲', '😟', '😦', '😧', '😈', '👿', '😮', '😬', '😐', '😕', '😯', '😶', '😇', '😏', '😑', '👲', '👳', '👮', '👷', '💂', '👶', '👦', '👧', '👨', '👩', '👴', '👵', '👱', '👼', '👸', '😺', '😸', '😻', '😽', '😼', '🙀', '😿', '😹', '😾', '👹', '👺', '🙈', '🙉', '🙊', '💀', '👽', '💩', '🔥', '✨', '🌟', '💫', '💥', '💢', '💦', '💧', '💤', '💨', '👂', '👀', '👃', '👅', '👄', '👍', '👎', '👌', '👊', '✊', '✌', '👋', '✋', '👐', '👆', '👇', '👉', '👈', '🙌', '🙏', '☝', '👏', '💪', '🚶', '🏃', '💃', '👫', '👪', '👬', '👭', '💏', '💑', '👯', '🙆', '🙅', '💁', '🙋', '💆', '💇', '💅', '👰', '🙎', '🙍', '🙇', '🎩', '👑', '👒', '👟', '👞', '👡', '👠', '👢', '👕', '👔', '👚', '👗', '🎽', '👖', '👘', '👙', '💼', '👜', '👝', '👛', '👓', '🎀', '🌂', '💄', '💛', '💙', '💜', '💚', '❤', '💔', '💗', '💓', '💕', '💖', '💞', '💘', '💌', '💋', '💍', '💎', '👤', '👥', '💬', '👣', '💭', '🐶', '🐺', '🐱', '🐭', '🐹', '🐰', '🐸', '🐯', '🐨', '🐻', '🐷', '🐽', '🐮', '🐗', '🐵', '🐒', '🐴', '🐑', '🐘', '🐼', '🐧', '🐦', '🐤', '🐥', '🐣', '🐔', '🐍', '🐢', '🐛', '🐝', '🐜', '🐞', '🐌', '🐙', '🐚', '🐠', '🐟', '🐬', '🐳', '🐋', '🐄', '🐏', '🐀', '🐃', '🐅', '🐇', '🐉', '🐎', '🐐', '🐓', '🐕', '🐖', '🐁', '🐂', '🐲', '🐡', '🐊', '🐫', '🐪', '🐆', '🐈', '🐩', '🐾', '💐', '🌸', '🌷', '🍀', '🌹', '🌻', '🌺', '🍁', '🍃', '🍂', '🌿', '🌾', '🍄', '🌵', '🌴', '🌲', '🌳', '🌰', '🌱', '🌼', '🌐', '🌞', '🌝', '🌚', '🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘', '🌜', '🌛', '🌙', '🌍', '🌎', '🌏', '🌋', '🌌', '🌠', '⭐', '☀', '⛅', '☁', '⚡', '☔', '❄', '⛄', '🌀', '🌁', '🌈', '🌊', '🎍', '💝', '🎎', '🎒', '🎓', '🎏', '🎆', '🎇', '🎐', '🎑', '🎃', '👻', '🎅', '🎄', '🎁', '🎋', '🎉', '🎊', '🎈', '🎌', '🔮', '🎥', '📷', '📹', '📼', '💿', '📀', '💽', '💾', '💻', '📱', '☎', '📞', '📟', '📠', '📡', '📺', '📻', '🔊', '🔉', '🔈', '🔇', '🔔', '🔕', '📢', '📣', '⏳', '⌛', '⏰', '⌚', '🔓', '🔒', '🔏', '🔐', '🔑', '🔎', '💡', '🔦', '🔆', '🔅', '🔌', '🔋', '🔍', '🛁', '🛀', '🚿', '🚽', '🔧', '🔩', '🔨', '🚪', '🚬', '💣', '🔫', '🔪', '💊', '💉', '💰', '💴', '💵', '💷', '💶', '💳', '💸', '📲', '📧', '📥', '📤', '✉', '📩', '📨', '📯', '📫', '📪', '📬', '📭', '📮', '📦', '📋', '📅', '📆', '📇', '📁', '📂', '✂', '📌', '📎', '✒', '✏', '📏', '📐', '📕', '📗', '📘', '📙', '📓', '📔', '📒', '📚', '📖', '🔖', '📛', '🔬', '🔭', '📰', '🎨', '🎬', '🎤', '🎧', '🎼', '🎵', '🎶', '🎹', '🎻', '🎺', '🎷', '🎸', '👾', '🎮', '🃏', '🎴', '🀄', '🎲', '🎯', '🏈', '🏀', '⚽', '⚾', '🎾', '🎱', '🏉', '🎳', '⛳', '🚵', '🚴', '🏁', '🏇', '🏆', '🎿', '🏂', '🏊', '🏄', '🎣', '☕', '🍵', '🍶', '🍼', '🍺', '🍻', '🍸', '🍹', '🍷', '🍴', '🍕', '🍔', '🍟', '🍗', '🍖', '🍝', '🍛', '🍤', '🍱', '🍣', '🍥', '🍙', '🍘', '🍚', '🍜', '🍲', '🍢', '🍡', '🍳', '🍞', '🍩', '🍮', '🍦', '🍨', '🍧', '🎂', '🍰', '🍪', '🍫', '🍬', '🍭', '🍯', '🍎', '🍏', '🍊', '🍋', '🍒', '🍇', '🍉', '🍓', '🍑', '🍈', '🍌', '🍐', '🍍', '🍠', '🍆', '🍅', '🌽', '🏠', '🏡', '🏫', '🏢', '🏣', '🏥', '🏦', '🏪', '🏩', '🏨', '💒', '⛪', '🏬', '🏤', '🌇', '🌆', '🏯', '🏰', '⛺', '🏭', '🗼', '🗾', '🗻', '🌄', '🌅', '🌃', '🗽', '🌉', '🎠', '🎡', '⛲', '🎢', '🚢', '⛵', '🚤', '🚣', '⚓', '🚀', '✈', '💺', '🚁', '🚂', '🚊', '🚉', '🚞', '🚆', '🚄', '🚅', '🚈', '🚇', '🚝', '🚋', '🚃', '🚎', '🚌', '🚍', '🚙', '🚘', '🚗', '🚕', '🚖', '🚛', '🚚', '🚨', '🚓', '🚔', '🚒', '🚑', '🚐', '🚲', '🚡', '🚟', '🚠', '🚜', '💈', '🚏', '🎫', '🚦', '🚥', '⚠', '🚧', '🔰', '⛽', '🏮', '🎰', '♨', '🗿', '🎪', '🎭', '📍', '🚩', '⬆', '⬇', '⬅', '➡', '🔠', '🔡', '🔤', '↗', '↖', '↘', '↙', '↔', '↕', '🔄', '◀', '▶', '🔼', '🔽', '↩', '↪', 'ℹ', '⏪', '⏩', '⏫', '⏬', '⤵', '⤴', '🆗', '🔀', '🔁', '🔂', '🆕', '🆙', '🆒', '🆓', '🆖', '📶', '🎦', '🈁', '✳', '❇', '❎', '✅', '✴', '💟', '💠', '➿', '♻', '™', '〰', '🔝', '🔚', '🔙', '🔛', '🔜', '❌', '⭕', '❗', '❓', '❕', '❔', '🔃', '💮', '💯',
+];
+
+// generate random emoji
+function setEmoji() {
+    emojiField.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+}
+
+// init count for random loop
+var i = 0;
+
+// random loop for changing the name
+function randomLoop() {
+    setTimeout(function () {
+        let length = listForRandomization.length;
+        let j = getRandomInt(length);
+        let randomName = listForRandomization[j];
+        randomNames.innerText = randomName;    
+        i++;
+        // random loop calling itself twelve times
+        if (i < 12) {
+            randomLoop();
+        } else {
+            moderator.prepend('Moderator/in heute:');
+            setEmoji();
+        }
+    }, 300)
+}
